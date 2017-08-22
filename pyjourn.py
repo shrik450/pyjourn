d8600656783261add6783cbb1f3f4afa59103598
shrik450 <shrik450@gmail.com>
2017-08-22 17:25:28 +0530

    Added windowclasses.

--- pyjourn.py
+++ pyjourn.py
@@ -8,6 +8,8 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+from windowclasses import *
+
 class PyJournRoot(tk.Frame):
 	def __init__(self, master):
 		self.master = master
@@ -16,7 +18,7 @@ class PyJournRoot(tk.Frame):
 		menubar = tk.Menu(master)
 
 		filemenu = tk.Menu(menubar, tearoff=0)
-		filemenu.add_command(label="New Journal", command=self.new_journal)#, state=tk.DISABLED)
+		filemenu.add_command(label="New Journal", command=self.create_new_journal)#, state=tk.DISABLED)
 		filemenu.add_command(label="Open Journal", command=self.donothing, state=tk.DISABLED)
 		filemenu.add_separator()
 		filemenu.add_command(label="New Entry", command=self.donothing, state=tk.DISABLED)
@@ -28,61 +30,11 @@ class PyJournRoot(tk.Frame):
 
 		master.config(menu=menubar)
 
-	def encrypt_and_writelines(self, file, lines):
-		outstream = []
-		for line in lines:
-			outstream.append(self.session_fernet.encrypt(str(line).encode('utf-8')))
+		self.session_journal = None
+		self.session_fernet = None
 
-		file.writelines(outstream)
-
-	def new_journal(self):
-		self.file_dialog = tk.Toplevel()
-		self.file_dialog.title("New Journal")
-
-		self.prompt = tk.Label(file_dialog, text="Create a New Journal")
-		self.prompt.grid(row=0, column=0, columnspan=4)
-
-		name_prompt = tk.Label(file_dialog, text="Name:")
-		name_prompt.grid(row=1, column=0)
-
-		self.name_box = tk.Entry(file_dialog)
-		self.name_box.grid(row=1, column=1, columnspan=2)
-
-		password_prompt = tk.Label(file_dialog, text="Password")
-		password_prompt.grid(row=2, column=0)
-
-		self.password_box = tk.Entry(file_dialog, show="*")
-		self.password_box.grid(row=2, column=1, columnspan=2)
-
-		create_button = tk.Button(file_dialog, text="Create", command=self.create_journal)
-		create_button.grid(row=1, column=3, rowspan=2)
-
-	def create_journal(self):
-		try:
-			self.journal_name = self.name_box.get()
-			password = self.password_box.get()
-
-			salt = os.urandom(16)
-			kdf = PBKDF2HMAC(
-				algorithm=hashes.SHA256,
-				length=32,
-				salt=salt,
-				iterations=100000,
-				backend=default_backend()
-			)
-			key = base64.urlsafe_b64encode(kdf.derive(password.encode('utf-8')))
-			self.session_fernet = fernet(key)
-
-			os.makedirs(self.journal_name)
-			self.journal_index = open(self.journal_name + ".pji", "w+b")
-
-			self.journal_index.write((str(salt)+"\n").encode('utf-8'))
-			preamble = [(self.journal_name+"\n").encode('utf-8'), (getpass.getuser()+"\n").encode('utf-8'), (time.strftime("%x") + "\n").encode('utf-8'), (time.strftime("%X") + "\n").encode('utf-8')]
-			self.encrypt_and_writelines(self.journal_index, preamble)
-
-			self.file_dialog.destroy()
-		except OSError:
-			self.prompt["text"] = "A Journal with this name already exists. Please use another name."
+	def create_new_journal(self):
+		new_journal_window = new_journal(self)	
 
 	def new_entry(self):
 		entry_window = tk.Toplevel()
@@ -109,20 +61,7 @@ class PyJournRoot(tk.Frame):
 		#self.name_box.grid(row=2, column=2, columnspan=6)
 		self.name_box.pack()
 
-		entry_frame = tk.Frame(entry_window)
-		#entry_frame.grid(row=3, column=0, columnspan=8)
-		entry_frame.pack()
-
-		self.entry_box = tk.Text(entry_frame, height=16, width=80)
-		#self.entry_box.grid(row=0, column=0)
-		self.entry_box.pack(side=tk.LEFT, fill=tk.Y)		
-
-		entry_box_scrollbar = tk.Scrollbar(entry_frame)
-		#entry_box_scrollbar.grid(row=0, column=1)
-		entry_box_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
-		entry_box_scrollbar.config(command=self.entry_box.yview)
-		self.entry_box.config(yscrollcommand=entry_box_scrollbar.set)
+		
 
 		entry_create_button = tk.Button(entry_window, text="Create", command=self.donothing)
 
@@ -130,7 +69,6 @@ class PyJournRoot(tk.Frame):
 
 		entry_text = self.entry_box.get()
 		outstream = self.session_fernet.encrypt(entry_text.encode('utf-8'))
-		self.
 
 	def donothing(self):
 		return 0
---
+++ windowclasses.py
@@ -0,0 +1,73 @@
+import base64
+import os
+import getpass
+import tkinter as tk
+import time
+from cryptography.fernet import Fernet as fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+def encrypt_and_writelines(file, lines, session_fernet):
+	outstream = []
+	for line in lines:
+		outstream.append(session_fernet.encrypt(str(line).encode('utf-8')))
+		file.writelines(outstream)
+
+class new_journal():
+	def __init__(self, master):
+		self.master = master
+
+		file_dialog = tk.Toplevel()
+		file_dialog.title("New Journal")
+
+		self.prompt = tk.Label(file_dialog, text="Create a New Journal")
+		self.prompt.grid(row=0, column=0, columnspan=4)
+
+		name_prompt = tk.Label(file_dialog, text="Name:")
+		name_prompt.grid(row=1, column=0)
+
+		self.name_box = tk.Entry(file_dialog)
+		self.name_box.grid(row=1, column=1, columnspan=2)
+
+		password_prompt = tk.Label(file_dialog, text="Password")
+		password_prompt.grid(row=2, column=0)
+
+		self.password_box = tk.Entry(file_dialog, show="*")
+		self.password_box.grid(row=2, column=1, columnspan=2)
+
+		create_button = tk.Button(file_dialog, text="Create", command=self.create_journal)
+		create_button.grid(row=1, column=3, rowspan=2)	
+
+	def create_journal(self):
+		try:
+			journal_name = self.name_box.get()
+			password = self.password_box.get()
+
+			salt = os.urandom(16)
+			kdf = PBKDF2HMAC(
+				algorithm=hashes.SHA256,
+				length=32,
+				salt=salt,
+				iterations=100000,
+				backend=default_backend()
+			)
+			key = base64.urlsafe_b64encode(kdf.derive(password.encode('utf-8')))
+			session_fernet = fernet(key)
+
+			os.makedirs(journal_name)
+			journal_index = open(journal_name + ".pji", "w+b")
+
+			journal_index.write((str(salt)+"\n").encode('utf-8'))
+			preamble = [(journal_name+"\n").encode('utf-8'), (getpass.getuser()+"\n").encode('utf-8'), (time.strftime("%x") + "\n").encode('utf-8'), (time.strftime("%X") + "\n").encode('utf-8')]
+			encrypt_and_writelines(journal_index, preamble, session_fernet)
+
+			file_dialog.destroy()
+			journal_index.close()
+
+			self.master.session_journal = journal_name
+			self.master.session_fernet = session_fernet
+
+		except OSError:
+			self.prompt["text"] = "A Journal with this name already exists. Please use another name."
+
