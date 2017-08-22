3ddf6f8eea1b78430216e1bb0fb4341947b8a48d
shrik450 <shrik450@gmail.com>
2017-08-22 15:52:38 +0530

    Added New Journal dialog

--- pyjourn.py
+++ pyjourn.py
@@ -15,7 +15,7 @@ class PyJournRoot(tk.Frame):
 		menubar = tk.Menu(master)
 
 		filemenu = tk.Menu(menubar, tearoff=0)
-		filemenu.add_command(label="New Journal", command=self.donothing, state=tk.DISABLED)
+		filemenu.add_command(label="New Journal", command=self.create_journal)#, state=tk.DISABLED)
 		filemenu.add_command(label="Open Journal", command=self.donothing, state=tk.DISABLED)
 		filemenu.add_separator()
 		filemenu.add_command(label="Exit", command=master.quit)
@@ -24,7 +24,31 @@ class PyJournRoot(tk.Frame):
 
 		master.config(menu=menubar)
 
-	def donothing():
+	def create_journal(self):
+		file_dialog = tk.Toplevel()
+		file_dialog.title("New Journal")
+
+		self.prompt = tk.Label(file_dialog, text="Create a New Journal")
+		self.prompt.grid(row=0, column=0, columnspan=4)
+
+		name_prompt = tk.Label(file_dialog, text="Name:")
+		name_prompt.grid(row=1, column=0)
+
+		self.name_box = tk.Entry(file_dialog)
+		self.name_box.grid(row=1, column=1, columnspan=2)
+
+		password_prompt = tk.Label(file_dialog, text="Password")
+		password_prompt.grid(row=2, column=0)
+
+		self.password_box = tk.Entry(file_dialog)
+		self.password_box.grid(row=2, column=1, columnspan=2)
+
+		create_button = tk.Button(file_dialog, text="Create", command=self.donothing())
+		create_button.grid(row=1, column=3, rowspan=2)
+
+
+
+	def donothing(self):
 		return 0
 
 window_root = tk.Tk()
